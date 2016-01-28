ab7259320c7dd8f5ecbb1cf69e38f63b2c0d7662
Joni Nousiainen <joni.nousiainen@codecenter.fi>
2016-01-29 00:22:48 +0200

    Added first puzzle.

--- js.js
+++ js.js
@@ -5,6 +5,24 @@ function showPart (part) {
 
 $(document).ready(function () {
 
-    showPart('doubt');
+    $('#birthYear').bind("change paste keyup", function() {
+        if ('' == $(this).val()) {
+            $('#mystery1 .alert-success').addClass('hidden');
+            $('#mystery1 .alert-danger').addClass('hidden');
+            $('#mystery1 .forward').prop('disabled', true);
+        }
+        else if ('1984' == $(this).val()) {
+            $('#mystery1 .alert-success').removeClass('hidden');
+            $('#mystery1 .alert-danger').addClass('hidden');
+            $('#mystery1 .forward').prop('disabled', false);
+        }
+        else {
+            $('#mystery1 .alert-success').addClass('hidden');
+            $('#mystery1 .alert-danger').removeClass('hidden');
+            $('#mystery1 .forward').prop('disabled', true);
+        }
+    });
+
+    showPart('mystery1');
 
 });
